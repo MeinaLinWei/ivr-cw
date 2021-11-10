0b6732ae8bf86d9275d66fb3664fc5f1c24361b5
MeinaLinWei <meina.lw12@gmail.com>
2021-11-10 22:57:49 +0000

    Add required packages

---
+++ catkin_ws/build/ivr_assignment/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/meina_lw12/ivr-cw/catkin_ws/src/ivr_assignment
+# Build directory: /home/meina_lw12/ivr-cw/catkin_ws/build/ivr_assignment
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
